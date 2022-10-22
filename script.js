44b0918c4ce2021718dac085bc482324dd967967
xndcmd <xndcmd@gmail.com>
2022-10-22 11:14:17 -0700

    map successfully renders + changed default mapview to westcoast

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 // -------- Javascript Page -------- \\
 // --------------------------------- \\
 
+
+var map = L.map('map').setView([41.1, -125], 5);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
